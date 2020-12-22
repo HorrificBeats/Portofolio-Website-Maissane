c3eed61008dfb6e68bbdc0719f522875e803ae51
Horia Gheorghe <horia_dude@yahoo.com>
2020-12-22 18:55:52 +0100

    Pre-Cleaning

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+/* function clicked() {
+    if(document.getElementsByClassName('Clicked').classList.contains('active')) {
+        document.getElementsByClassName('Clicked').classList.remove('active');
+    } else {
+        document.getElementsByClassName('Clicked').classList.add('active');
+    }   
+} */
+
+/* OwlCarousel2 */
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel();
+  });
+
+
+
+/**
+ * * Highlihght
+ * ! Danger
+ * ? Questions
+ * TODO: blabla
+ */
