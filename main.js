4552fa832af03de2dd0887371e352dd0531103e7
Horia Gheorghe <horia_dude@yahoo.com>
2021-01-08 10:45:02 +0100

    Done: blue cursor, missing projects, added jquery to all projects, extra comments

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 //Fonction JS pour cacher/afficher le "scroll hint" sur la page d'accueil
-function hideScrollHint(ev){
-    if(window.pageYOffset>500){
-        document.getElementById("scrollHint").classList.add("d-none");
-    } else {
-        document.getElementById("scrollHint").classList.remove("d-none");
-    }
+function hideScrollHint(ev) {
+  if (window.pageYOffset > 500) {
+    document.getElementById("scrollHint").classList.add("d-none");
+  } else {
+    document.getElementById("scrollHint").classList.remove("d-none");
+  }
 }
 
 //Demarer la fonction lorsque l'utilisateur fait défiler la page
-window.onscroll=hideScrollHint; 
-
+window.onscroll = hideScrollHint;
 
 /* Movement automatique carousel Bootstrap */
-/* $(window).load(function() {
-    $('.carousel').carousel('pause'); 
-}); */
-
-$(document).ready(function(){
-    //Event for pushed the video
-    $('#your-carousel-id').carousel({
-        pause: true,
-        interval: false
-    });
+/* Désactivé avec jQuery */
+$(document).ready(function () {
+  //Event for pushed the video
+  $("#your-carousel-id").carousel({
+    pause: true,
+    interval: false,
+  });
 });
-
