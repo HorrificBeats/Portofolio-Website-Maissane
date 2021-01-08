ba39ecc263980e0a7195c71f5f9a8884cb35d976
Horia Gheorghe <horia_dude@yahoo.com>
2021-01-08 10:19:34 +0100

    Done: Client Presentation

--- main.js
+++ main.js
@@ -1,57 +1,26 @@
-/* $(document).ready(function () {
-  jAlert("Example of a basic alert box in jquery", "jquery basic alert box");
-}); */
-
-/* $(window).scroll(function() {
-
-    if ($(this).scrollTop()>0)
-     {
-        $('.a').fadeOut();
-     }
-    else
-     {
-      $('.a').fadeIn();
-     }
- });
- */
-/* $(window).scroll(function () {
-    var Bottom = $(window).height() + $(window).scrollTop() >= $(document).height();
-  if(Bottom )
-  {
-  $('.a').hide();
-  }
-  }); */
-
-
-
-
-
-  /* window.onload = function() {
-alert("Hello! I am an alert box!!");  
-} */
-/* function Scroll() { 
-    alert("Hello! I am an alert box!!"); 
-    window.scroll(200, 900,); 
-} */
-
-/* window.onscroll(function() {
-    document.getElementsByClassName("ups").style.display = "none";
-}); */
-
-
-function testScroll(ev){
-    /* var Pula = document.getElementsByClassName('ups'); */
-    if(window.pageYOffset>2){
-        /* alert('User has scrolled at least 400 px!'); */
-        document.getElementById("ups").classList.add("fadeOutCLASS");
-        document.getElementById("ups").classList.add("d-none");
-        /* Pula.ClassList.remove('') */
-    } else /* if (window.pageYOffset>1) */ {
-        document.getElementById("ups").classList.remove("d-none");
-        document.getElementById("ups").classList.remove("fadeOutCLASS");
-        
+//Fonction JS pour cacher/afficher le "scroll hint" sur la page d'accueil
+function hideScrollHint(ev){
+    if(window.pageYOffset>500){
+        document.getElementById("scrollHint").classList.add("d-none");
+    } else {
+        document.getElementById("scrollHint").classList.remove("d-none");
     }
 }
-window.onscroll=testScroll
 
+//Demarer la fonction lorsque l'utilisateur fait défiler la page
+window.onscroll=hideScrollHint; 
+
+
+/* Movement automatique carousel Bootstrap */
+/* $(window).load(function() {
+    $('.carousel').carousel('pause'); 
+}); */
+
+$(document).ready(function(){
+    //Event for pushed the video
+    $('#your-carousel-id').carousel({
+        pause: true,
+        interval: false
+    });
+});
 
